c0c2f469c581487119d83c379a9b534d9ce89398
Rebeca Rodríguez <rebeca.rodriguez.guirao@gmail.com>
2024-03-10 22:24:07 +0100

    generate component films-details

---
+++ src/app/components/films-details/films-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-films-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './films-details.component.html',
+  styleUrl: './films-details.component.scss'
+})
+export class FilmsDetailsComponent {
+
+}
